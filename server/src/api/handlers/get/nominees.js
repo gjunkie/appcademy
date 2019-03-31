8dc464abc99199ec7369239c6952e0be734c0265
Freddie Carthy <fcarthy@gmail.com>
2019-03-30 23:02:37 -0700

    modify nominees endpoint to get films and artists

--- server/src/api/handlers/get/nominees.js
+++ server/src/api/handlers/get/nominees.js
@@ -1,6 +1,22 @@
 import Boom from 'boom';
 
-const getNominees = request => (
+const getArtists = request => (
+  new Promise(resolve => (
+    request.server.plugins.db.Artist
+      .find({})
+      .exec((err, artists) => {
+        if (err) {
+          const error = Boom.badRequest('Invalid query');
+          error.output.payload.info = error;
+          resolve(error);
+        }
+
+        resolve(artists);
+      })
+  ))
+);
+
+const getFilms = request => (
   new Promise(resolve => (
     request.server.plugins.db.Film
       .find({})
@@ -14,6 +30,18 @@ const getNominees = request => (
         resolve(films);
       })
   ))
+);
+
+const getNominees = request => (
+  new Promise((resolve) => {
+    getFilms(request).then((films) => {
+      const first = [].concat(films);
+      getArtists(request).then((artists) => {
+        const nominees = first.concat(artists);
+        resolve(nominees);
+      });
+    });
+  })
 );
 
 export default getNominees;
